d9071972c3adbfc01852c4e0cad3dec519dd68c3
Konstantin <krash7087@gmail.com>
2021-06-28 13:43:21 +0300

    Initial Commit

---
+++ 2 Dogs or Cats list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2 Dogs or Cats list
+//
+//  Created by Константин Крашенинников on 28.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
